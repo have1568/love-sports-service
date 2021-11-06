a0f38fb5829af790ed9b03dae21741e7de0042c4
have1568 <1006369654@qq.com>
2021-11-07 01:12:01 +0800

    init:网关、用户模块创建。引入Nacos源码启动本地Nacos服务

---
+++ love-sports-gateway/src/test/java/com/love/sports/LoveSportsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveSportsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ love-sports-user/src/main/java/com/love/sports/user/LoveSportsUserApplication.java
@@ -0,0 +1,13 @@
+package com.love.sports.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveSportsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveSportsUserApplication.class, args);
+    }
+
+}
